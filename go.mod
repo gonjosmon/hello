58d1ebeac4cb55dfd58c9f5df3f1bee8d6b5be24
gonjosmon <gonjosmon@gmail.com>
2022-01-26 19:34:18 -0300

    funciona!

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module example/user/hello
 
 go 1.17
+
+require (
+	example/user/greetings v0.0.0-00010101000000-000000000000
+	github.com/google/go-cmp v0.5.7
+)
+
+replace example/user/greetings => ../greetings
--- hello.go
+++ hello.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"example/user/greetings"
+	"example/user/hello/morestrings"
+	"fmt"
+
+	"github.com/google/go-cmp/cmp"
+)
 
 func main() {
 	fmt.Println("Hello, world.")
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
+
+	message := greetings.Hello("Gonza")
+	fmt.Println(message)
 }
