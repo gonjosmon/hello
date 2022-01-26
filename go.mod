cabdada548eeccab26b8e63372f320b2a59bc2e6
gonjosmon <gonjosmon@gmail.com>
2022-01-26 19:57:43 -0300

    metí todo adentro de hello

--- go.mod
+++ go.mod
@@ -2,9 +2,4 @@ module example/user/hello
 
 go 1.17
 
-require (
-	example/user/greetings v0.0.0-00010101000000-000000000000
-	github.com/google/go-cmp v0.5.7
-)
-
-replace example/user/greetings => ../greetings
+require github.com/google/go-cmp v0.5.7
--- hello.go
+++ hello.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"example/user/greetings"
+	"example/user/hello/greetings"
 	"example/user/hello/morestrings"
 	"fmt"
+	"log"
 
 	"github.com/google/go-cmp/cmp"
 )
@@ -12,7 +13,23 @@ func main() {
 	fmt.Println("Hello, world.")
 	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
 	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
+	fmt.Println("")
 
-	message := greetings.Hello("Gonza")
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Request a greeting message.
+	message, err := greetings.Hello("Gon")
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message
+	// to the console.
 	fmt.Println(message)
 }
